ad2cb8f71c579ce56930eda60061ee050a160180
priyanka <ubhepriyanka243@gmail.com>
2022-07-30 22:09:26 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using InterfaceTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace InterfaceTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+            [HttpPost]
+        public IActionResult Privacy(Values v)
+        {
+            FileStream fs = new FileStream(@"D:\InterfaceTask.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                try
+            {
+                v.Add = v.a + v.b + v.c;
+                v.Multi = v.a * v.b * v.c;
+
+                XmlSerializer xs = new XmlSerializer(typeof(Values));
+                xs.Serialize(fs, v);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+            return View();
+        }
+
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+}
---
+++ Models/Maths.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InterfaceTask.Models
+{
+    public class Values
+    {
+        [Required]
+        public int a { get; set; }
+
+        [Required]
+        public int b { get; set; }
+        [Required]
+        public int c { get; set; }
+
+        public int Add;
+        public int Multi;
+    }
+    public interface Math
+    {
+        public int Add(int a, int b, int c);
+        public int Multi(int a, int b, int c);
+
+    }
+
+public class IMaths : Math
+{
+    public int Add(int a, int b,int c)
+    {
+        int ans = a + b;
+        return ans;
+    }
+
+    public int Multi(int a, int b,int c)
+    {
+        int ans = a * b;
+        return ans;
+    }
+}
+}
